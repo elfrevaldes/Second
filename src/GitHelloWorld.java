d187c4d03d299153f0186e7e37f808b6654afe4c
elfrevaldes <val11005@byui.edu>
2014-05-14 23:16:09 -0600

    I had to change some things

--- src/GitHelloWorld.java
+++ src/GitHelloWorld.java
@@ -1,11 +1,7 @@
 
 public class GitHelloWorld {
 	public static void main(String [] argv){
-<<<<<<< HEAD
 		System.out.println("Hello GitHub");
-=======
-		System.out.println("Hello GitHub World");
 		System.out.println("Testing...");
->>>>>>> origin/Test
 	}
 }
