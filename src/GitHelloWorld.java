3d61cc778e3625a579cfff53b185eacea7e1e5c8
elfrevaldes <val11005@byui.edu>
2014-05-14 23:44:01 -0600

    I add another line
    I add one more line so that I can merge again

--- src/GitHelloWorld.java
+++ src/GitHelloWorld.java
@@ -3,5 +3,6 @@ public class GitHelloWorld {
 	public static void main(String [] argv){
 		System.out.println("Hello GitHub World");
 		System.out.println("Testing...");
+		System.out.println("one more change to test");
 	}
 }
